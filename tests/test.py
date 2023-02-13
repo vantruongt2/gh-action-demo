09be4208697054a6ad34ba2fbf89d2a5a9793b72
Truong Pham <truong.external@dnascript.co>
2023-02-13 15:38:17 +0700

    test

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class TestPage(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            options=webdriver.ChromeOptions()
+        )
+
+    def test_content(self):
+        self.driver.get('https://vantruongt2.github.io/gh-action-demo/')
+        ele = self.driver.find_element(By.ID, "wf")
+        self.assertEqual(ele.text, 'Workflows')
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
